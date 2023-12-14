8e9317b5a04ae64fc5500a3cded8733e4f3da603
Uku <149318155+Ukutktk@users.noreply.github.com>
2023-12-14 17:04:57 +0200

    Add files via upload

---
+++ DayOfTheWeek/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+using System.Collections.Concurrent;
+
+int Weekday = Convert.ToInt32(DateTime.Now.DayOfWeek);
+
+Console.WriteLine(Weekday);
+
+if (Weekday == 0)
+{
+    Console.WriteLine("It's Sunday ");
+}
+else if (Weekday == 1)
+{
+    Console.WriteLine("It's Monday");
+}
+else if (Weekday == 2)
+{
+    Console.WriteLine("It's Tuesday");
+}
+else if(Weekday == 3)
+{
+    Console.WriteLine("It's Wednesday");
+}
+else if (Weekday == 4)
+{
+    Console.WriteLine("It's Thrusday");
+}
+else if (Weekday == 5)
+{
+    Console.WriteLine("It's Friday");
+}
+else if (Weekday == 6)
+{
+    Console.WriteLine("It's Saturday");
+}
+
+Console.WriteLine("Have a nice day");
---
+++ SwitchDayOfTheWeek/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using System.Collections.Concurrent;
+
+int Weekday = Convert.ToInt32(DateTime.Now.DayOfWeek); // 0 - 6
+
+switch(Weekday) //väärtus, mida kontrolitakse
+{
+    case 0: //case 0: == if(Weekday == 0)
+        Console.WriteLine("It's Sunday");
+        break;
+    case 1:
+        Console.WriteLine("It's Monday");
+        break;
+    case 2:
+        Console.WriteLine("It's Tuesday");
+        break;
+    case 3:
+        Console.WriteLine("It's Wednesday");
+        break;
+    case 4:
+        Console.WriteLine("It's Thrusday");
+        break;
+    case 5:
+        Console.WriteLine("It's Friday");
+        break;
+    case 6:
+        Console.WriteLine("It's Saturday");
+        break;
+    default:
+        Console.WriteLine("Ooop. Your calender must be broken.");
+        break;
+}
+
+Console.WriteLine("Have a nice day");
